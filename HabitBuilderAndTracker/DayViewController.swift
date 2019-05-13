27e2076299fc30de4e0f58c463667119bc3f513b
Ed Woulfe <edwoulfe@gmail.com>
2019-05-13 16:18:32 -0500

    Fixed constraints on add habit view controller. Button now segues back into the habit table view. Added variables to the edit view controller to hold the values of days selected and habit name.

--- HabitBuilderAndTracker/DayViewController.swift
+++ HabitBuilderAndTracker/DayViewController.swift
@@ -8,48 +8,19 @@
 
 import UIKit
 
-class Habits{
-    var habitName: String?
-    var habitDuration: String?
-    
-    init(hName:String, hDuration:String) {
-        self.habitName = hName
-        self.habitDuration = hDuration
-    }
-    
-}
 
 class DayViewController: UIViewController{
     
     @IBOutlet weak var addNewHabitButton: UIButton!
-    
+
+
   
-    //
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 1
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        var cell = tableView.dequeueReusableCell(withIdentifier: "habitsTable")
-//        if cell == nil{
-//            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "habitsTable")
-//        }
-//        cell?.textLabel?.text = "Gym"
-//        cell?.detailTextLabel?.text = "Weekdays"
-//
-//        return cell!
-//
-//    }
-//
    @IBOutlet weak var habitTable: UITableView!
     @IBAction func backToCalendarButtonPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: {});
         self.navigationController?.popViewController(animated: true);
     }
-    //
-//
-//    var habitArray = [Habits]()
-//
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
--- HabitBuilderAndTracker/newHabitViewController.swift
+++ HabitBuilderAndTracker/newHabitViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class newHabitViewController: UIViewController{
+    public var parentView: DayViewController!
+    var monday = false;
+    var tuesday = false;
+    var wednesday = false;
+    var thursday = false;
+    var friday = false;
+    var saturday = false;
+    var sunday = false;
+    public var update: ((String, Bool, Bool, Bool, Bool, Bool, Bool, Bool) ->())!
+    
+    @IBOutlet weak var habitNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,16 +35,20 @@ class newHabitViewController: UIViewController{
     @IBAction func mondayButtonSelected(_ sender: UIButton) {
         if sender.isSelected {
             sender.isSelected = false
+            monday = false
         }else{
             sender.isSelected = true
+            monday = true
         }
     }
     
     @IBAction func tuesdayButtonSelected(_ sender: UIButton) {
         if sender.isSelected {
             sender.isSelected = false
+            tuesday = false
         }else{
             sender.isSelected = true
+            tuesday = true
         }
     }
     
@@ -40,16 +56,20 @@ class newHabitViewController: UIViewController{
     @IBAction func wednesdayButtonSelected(_ sender: UIButton) {
         if sender.isSelected {
             sender.isSelected = false
+            wednesday = false
         }else{
             sender.isSelected = true
+            wednesday = true
         }
     }
     
     @IBAction func thursdayButtonSelected(_ sender: UIButton) {
         if sender.isSelected {
             sender.isSelected = false
+            thursday = false
         }else{
             sender.isSelected = true
+            thursday = true
         }
     }
     
@@ -57,26 +77,38 @@ class newHabitViewController: UIViewController{
     @IBAction func fridayButtonSelected(_ sender: UIButton) {
         if sender.isSelected {
             sender.isSelected = false
+            friday = false
         }else{
             sender.isSelected = true
+            friday = true
         }
     }
     @IBAction func saturdayButtonSelected(_ sender: UIButton) {
         if sender.isSelected {
             sender.isSelected = false
+            saturday = false
         }else{
             sender.isSelected = true
+            saturday = true
         }
     }
     
     @IBAction func sundayButtonSelected(_ sender: UIButton) {
         if sender.isSelected {
             sender.isSelected = false
+            sunday = false
         }else{
             sender.isSelected = true
+            sunday = true
         }
     }
     
+   
     
+    @IBAction func habitButtonPressed(_ sender: UIButton) {
+        update(habitNameTextField.text ?? "", monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+        self.dismiss(animated: true, completion: {});
+        self.navigationController?.popViewController(animated: true);
+    }
     
 }
